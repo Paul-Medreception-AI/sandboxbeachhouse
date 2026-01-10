5535694b2bbfca96368d9c3d232229ce26c05e16
Paul DevOps <paultoomeymd@medreception.ai>
2026-01-10 09:23:32 -0500

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KSMQ7QLN');`,
+})(window,document,'script','dataLayer','GTM-KLDWLWNW');`,
           }}
         />
         <script
@@ -83,7 +83,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KSMQ7QLN"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLDWLWNW"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
